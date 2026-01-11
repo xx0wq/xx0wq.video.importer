5b093874e7b2159c16bfc077d82f8d03852d19fa
Denys3Kopiuk22 <160676944+xx0wq@users.noreply.github.com>
2026-01-11 17:39:09 +0200

    release 0.70.93

---
+++ setup/include/Geode/Modify.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "modify/Modify.hpp"
+
+#include <Geode/GeneratedModify.hpp>
+
+using namespace geode::modifier;
---
+++ setup/include/Geode/Utils.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "DefaultInclude.hpp"
+#include "utils/VersionInfo.hpp"
+#include "utils/ranges.hpp"
+#include "utils/casts.hpp"
+#include "utils/cocos.hpp"
+#include "utils/map.hpp"
+#include "utils/string.hpp"
+#include "utils/file.hpp"
+#include "utils/permission.hpp"
+#include "utils/general.hpp"
+#include "utils/timer.hpp"
+#include "utils/ObjcHook.hpp"
